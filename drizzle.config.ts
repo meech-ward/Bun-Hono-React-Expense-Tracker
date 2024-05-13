079ff636916e5237a3e0da2c24f47a3845d4b6be
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2024-05-13 19:07:41 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./server/db/expenses.ts",
+  schema: "./server/db/schema/expenses.ts",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
